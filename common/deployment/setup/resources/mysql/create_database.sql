67dd4cb59759a6bf384b9338e297e2b1112f8231
AmshikaH <66556626+AmshikaH@users.noreply.github.com>
2025-06-04 10:39:01 +0530

    Update create_database.sql

--- common/deployment/setup/resources/mysql/create_database.sql
+++ common/deployment/setup/resources/mysql/create_database.sql
@@ -2,7 +2,7 @@ create database IDENTITY_DB;
 create database UM_DB;
 create database REG_DB;
 
-use IDENTITY_DB; source ~/wso2is/dbscripts/identity/mysql-cluster.sql;
+use IDENTITY_DB; source ~/wso2is/dbscripts/identity/mysql.sql;
 use IDENTITY_DB; source ~/wso2is/dbscripts/consent/mysql.sql;
 use UM_DB; source ~/wso2is/dbscripts/mysql.sql;
 use REG_DB; source ~/wso2is/dbscripts/mysql.sql;
